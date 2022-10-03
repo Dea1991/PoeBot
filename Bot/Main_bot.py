fbe84d13019157db505c28407df62a6dc8fca367
Армен <erik_nsk@mail.ru>
2022-10-03 13:47:38 +0700

    создал скелет

---
+++ Bot/Main_bot.py
@@ -0,0 +1,17 @@
+import pyautogui as pg
+import pyperclip as pc
+import time
+import keyboard
+
+
+# 1 global_variable
+div_rate = 0
+
+# 2 parsing_currency
+
+# 3 monitoring_chat
+
+# 4 location_objects
+
+# 5 comparison
+
