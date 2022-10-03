46533c1a12309109832e40e9a3ad5fc65304f07b
Армен <erik_nsk@mail.ru>
2022-10-03 13:27:14 +0700

    1

---
+++ PoeBot/main.py
@@ -0,0 +1,58 @@
+import pyautogui as pg
+import pyperclip as pc
+import time
+import keyboard
+x = 0
+
+
+def roll_alt():
+    pg.moveTo(121, 277, 0.1)
+    pg.click(button='right')
+    pg.moveTo(361, 471, 0.1)
+    pg.click(button='left')
+    time.sleep(0.2)
+    pg.hotkey('ctrl', 'alt', 'c')
+    file = open('data.txt', "w", encoding='utf-8')
+    file.write(pc.paste())
+    file.close()
+    file1 = open('data.txt')
+    read_txt = file1.read()
+
+    if "Prefix" not in read_txt:
+        pg.moveTo(224, 325, 0.1)
+        pg.click(button='right')
+        pg.moveTo(361, 471, 0.1)
+        pg.click(button='left')
+        time.sleep(0.2)
+        pg.hotkey('ctrl', 'alt', 'c')
+        file1.close()
+        file2 = open('data.txt', "w", encoding='utf-8')
+        file2.write(pc.paste())
+        file2.close()
+
+
+a = 0
+count_alt = 0
+roll_alt()
+
+file3 = open('data.txt')
+read_txt1 = file3.read()
+if "Dictator's" in read_txt1:
+    a = 1
+    print("Merciless")
+if "Merciless" in read_txt1:
+    a = 1
+    print("Merciless")
+
+while a != 1:
+    roll_alt()
+    count_alt += 1
+    file3 = open('data.txt')
+    read_txt1 = file3.read()
+    if "Merciless" in read_txt1:
+        a = 1
+        print("Merciless")
+    if "Dictator's" in read_txt1:
+        a = 1
+        print("Dictator's")
+    print(count_alt)
---
+++ PoeBot/prob.py
@@ -0,0 +1,15 @@
+import pyautogui as pg
+
+print(pg.position())
+
+
+while a == 0:
+    roll_alt()
+else: print("rdy")
+
+
+#Dictator's
+#Merciless
+# Point(x=121, y=277) переменки
+# Point(x=361, y=471) предмет
+#Point(x=224, y=325) усиления
